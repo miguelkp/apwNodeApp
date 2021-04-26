f80bf5eec8847f72bb506e7544b0e26c85b50d94
miguelkp <78116526+miguelkp@users.noreply.github.com>
2021-04-26 11:33:39 -0400

    draft

--- models/user.js
+++ models/user.js
@@ -1,35 +1,64 @@
-var bcrypt = require("bcryptjs");
-var mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 
-const SALT_FACTOR = 10;
 
-var UserSchema = mongoose.Schema({
-    username:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    password:{type:String, required:false},
+const UserSchema = mongoose.Schema({
+    username:
+    {
+        type:String, 
+        required:true
+    },
+    email:
+    {
+        type:String, 
+        required:true, 
+        unique:true
+    },
+    password:
+    {
+        type:String, 
+        required:false
+    },
 });
 
-UserSchema.pre("save", function(done){
-    var user = this;
+// Prior to saving the User information into the database, we save the currentUser.
+UserSchema.pre("save", function(done)
+{
+    var currentUser = this;
 
-    if(!user.isModified("password")){
-        return done();
+    if(!currentUser.isModified("password"))
+    { // Parameter check to see if the password is modified
+        return done(); // Exits the function if the password isn't modified
     }
 
-    bcrypt.genSalt(SALT_FACTOR, function(err,salt){
-        if(err){return done(err);}
-        bcrypt.hash(user.password, salt, function(err, hashedPassword){
-            if(err) {return done(err);}
-            user.password = hashedPassword;
+    bcrypt.genSalt(10, function(err, salt) // Asynchronously generates a salt hash with a default of 10 rounds 
+    { 
+        if(err){
+            return done(err);
+        } 
+        
+        // If there are no errors, we hash the user's password 
+        bcrypt.hash(currentUser.password, salt, function(err, hashedPassword)
+        {
+            if(err) {
+                return done(err);
+            }
+            currentUser.password = hashedPassword; // Sets the user password to the hashed password generated above
             done();
         });
     });
 
 });
 
-UserSchema.methods.checkPassword = function(guess, done){
-      if(this.password != null){
-          bcrypt.compare(guess,this.password, function(err, isMatch){
+// Function to check password supplied to the hashed passwords saved in the database 
+UserSchema.methods.checkPassword = function(guess, done)
+{
+      if(this.password != null)
+      {
+          // Compares the text form of the password user has inputted with the hashed passwords stored
+          // in the database
+          bcrypt.compare(guess,this.password, function(err, isMatch)
+          {
              done(err, isMatch);
           });
       }
